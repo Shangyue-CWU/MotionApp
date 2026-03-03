a5ddd7b5624779cf683f3ee35fe3a8b6c71e7899
CindyChitsuwa <cindychitsuwa@icloud.com>
2026-03-02 17:58:27 -0800

    working version of the app includes : Tasha's Settings page            Cindy's History page            Everything from the previous week

---
+++ mobile/src/main/java/com/example/motionwatch/AnalyticsFragment.kt
@@ -0,0 +1,13 @@
+package com.example.motionwatch
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class AnalyticsFragment : Fragment(R.layout.fragment_analytics) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Currently blank as requested
+    }
+}
